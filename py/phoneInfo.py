de0563388a158a5fc7169ee84fadf8c4bea853d7
today <today.xa@gmail.com>
2012-07-25 16:42:37 +0800

    搭建机型 维护 功能程序框架完成。

--- py/phoneInfo.py
+++ py/phoneInfo.py
@@ -16,57 +16,28 @@ class uploadPhoneInfoPage:
 
 class addPhoneInfo:
     def GET(self):
+        runData = {'runFlag':True,}
+        runData['showMsg']= "网络入口不正确，请按照正常操作流程使用系统。" 
+        return render.err(runData) 
+
+
+    def POST(self):
         flag = True
-        renderData = {'runFlag':True,'showMsg':'OK'}
+        runData = {'runFlag':True,'showMsg':'OK'}
+
+        ''' 
+        先上传文件，再检查文件，
+        包括格式、内容、以及是否和数据库中已有内容冲突， 
+        如果不正确，报错，要求重新上传正确文件；
+        如果正确，把内容灌入DB。
+        灌入数据完成后，将全部机型代码显示在页面上。
+        '''
         
-        req = web.input()
-        
-        cx = lazy.getConn()
-        cu = cx.cursor()
+         
 
-        strSql = "SELECT data_no, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14 from temp_data WHERE data_type='fld' and use_flag='0' ORDER BY c1,c5" 
-        print strSql
-        cu.execute(strSql)
+        runData['showMsg'] = '增加机型信息成功'
 
-        dataCount = 0
-        prevNo = 'abcdefghijklmn'
-        thisNo = ''
-        for sqlrow in cu:
-            dataCount = dataCount + 1
-            renderData['c0_' +  str(dataCount) ] = sqlrow[0]
-            renderData['c1_' +  str(dataCount) ] = sqlrow[1]
-            renderData['c2_' +  str(dataCount) ] = sqlrow[2]
-            renderData['c3_' +  str(dataCount) ] = sqlrow[3]
-            renderData['c4_' +  str(dataCount) ] = sqlrow[4]
-            thisNo = sqlrow[5]
-            renderData['c5_' +  str(dataCount) ] = thisNo
-            renderData['c6_' +  str(dataCount) ] = sqlrow[6]
-            renderData['c7_' +  str(dataCount) ] = sqlrow[7]
-            renderData['c8_' +  str(dataCount) ] = sqlrow[8]
-            renderData['c9_' +  str(dataCount) ] = sqlrow[9]
-            renderData['c10_'+  str(dataCount) ] = sqlrow[10]
-            renderData['c11_'+  str(dataCount) ] = sqlrow[11]
-            renderData['c12_'+  str(dataCount) ] = sqlrow[12]
-            renderData['c13_'+  str(dataCount) ] = sqlrow[13]
-            renderData['c14_'+  str(dataCount) ] = sqlrow[14]
-
-            print "thisNo=" + thisNo
-            print "prevNo=" + prevNo
-            if cmp(prevNo, 'abcdefghijklmn')==0:
-                prevNo = thisNo
-            elif cmp(prevNo, thisNo) != 0 :
-                dataCount = dataCount - 1
-                break
-            elif dataCount == 10 :
-                break
-
-        cu.close()
-        cx.close()
-   
-
-
-
-        return render.makeFld(renderData)
+        return render.phoneInfo(runData)
 
         
 
