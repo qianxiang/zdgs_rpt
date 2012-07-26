18a921cc391cd47fb832d7dc0d87a6ebf98a49e1
today <today.xa@gmail.com>
2012-07-26 09:45:51 +0800

    机型维护——上传功能完成

--- py/phoneInfo.py
+++ py/phoneInfo.py
@@ -31,6 +31,26 @@ class addPhoneInfo:
         灌入数据完成后，将全部机型代码显示在页面上。
         '''
         
+        # 上传文件
+        x = web.input(myfile={})
+        filedir = "static/upload"
+        if 'myfile' in x: 
+            # replaces the windows-style slashes with linux ones.
+            filepath=x.myfile.filename.replace('\\','/')
+
+            # splits the and chooses the last part (the filename with extension)
+            filename=filepath.split('/')[-1] 
+            
+            # creates the file where the uploaded file should be stored
+            fullpath = filedir +'/'+ lazy.getYmdhms() + '.txt'
+            print fullpath
+            fout = open( fullpath,'wb') #使用 wb 是为了在windows下能正确的上传文件。
+            
+            # writes the uploaded file to the newly created file.
+            fout.write(x.myfile.file.read())            
+
+            fout.close()
+        # 去预览数据，并进行挑选
          
 
         runData['showMsg'] = '增加机型信息成功'
